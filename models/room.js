3db5b64c3cb627980656a811b761297e8c32b027
Joel Sequeira <joelseq96@gmail.com>
2017-04-03 17:09:55 -0700

    Add building info to room schema

--- models/room.js
+++ models/room.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const roomSchema = new Schema({
   name: { type: String, unique: true },
   times: [ { day: String, time: String } ],
+  building: { type: Object },
 });
 
 module.exports = mongoose.model('room', roomSchema);
