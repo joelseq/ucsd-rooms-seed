535e6187a70faba166593ca23d4273d5adf82c8e
Joel Sequeira <joelseq96@gmail.com>
2017-04-03 17:09:35 -0700

    Add file to get dept codes for selected quarter

---
+++ get_codes.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const jsonfile = require('jsonfile');
+
+const QUARTER = 'SP17';
+
+axios
+  .get(`https://act.ucsd.edu/scheduleOfClasses/subject-list.json?selectedTerm=${QUARTER}`)
+  .then((response) => {
+    const codes = response.data.map(elem => elem.code);
+
+    jsonfile.writeFile('./codes.json', codes, console.log);
+  })
+  .catch(console.log);
